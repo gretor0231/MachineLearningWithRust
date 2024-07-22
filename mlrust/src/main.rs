1d9b9a88d50ff9d0e339bfb2c8d0b417bc31994e
gretor0231 <gretor0231@gmail.com>
2024-07-22 13:48:32 +0800

    main: 1. sum and product of array.

--- mlrust/src/main.rs
+++ mlrust/src/main.rs
@@ -1,3 +1,22 @@
+extern crate ndarray;
+use ndarray::Array;
+
 fn main() {
+
     println!("Hello, world!");
+
+    let array1 = Array::from_shape_vec((2,2),vec![1.,2.,3.,4.]).unwrap();
+    let array2 = Array::from_shape_vec((2,2),vec![4.,3.,2.,1.]).unwrap();
+
+    println!("Array1:\n {:?}",array1);
+    println!("Array2:\n {:?}",array2);
+
+
+    let sum = &array1 + &array2;
+    println!("Sum:\n {:?}",sum);
+
+
+    let product = &array1 *(&array2);
+    println!("Product:\n {:?}",product);
+
 }
